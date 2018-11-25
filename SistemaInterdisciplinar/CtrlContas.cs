27dd88aed1673a019113fab9839a8664a0803194
Arthur Marinheiro Vilela <arthuramrinheirovilela@gmail.com>
2018-11-25 18:37:51 -0200

    Código/Mascara de conta

--- SistemaInterdisciplinar/CtrlContas.cs
+++ SistemaInterdisciplinar/CtrlContas.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.OleDb;
+using System.Windows.Forms;
 
 namespace SistemaInterdisciplinar
 {
@@ -31,17 +32,50 @@ namespace SistemaInterdisciplinar
             string text = "";
             string mascara = Configuracoes.mascaraInterna;
 
+            int[] niveis = { n1, n2, n3, n4, n5 };
+            string[] niveisStr = new string[5];
             string[] tokens = mascara.Split('-');
 
-            foreach (string token in tokens) {
-                if(token.Contains("9"))
+            int i = 0;
+            string chave = "";
+            char c;
+            foreach (int nivel in niveis)
+            {
+                chave = "";
+                
+                if (tokens[i].Contains("9"))
                 {
-
-                } else
+                    
+                    for (int j = 0; j < tokens[i].Length; j++)
+                    {
+                        chave += "0";
+                    }
+                    niveisStr[i] = string.Format("{0:"+ chave +"}", nivel);
+                } else if (tokens[i].Contains("l"))
                 {
-
+                    c = (char) (nivel + 'a');
+                    chave = c.ToString();
                 }
+                i++;
             }
+
+            if (n2 == 0)
+            {
+                text = String.Format("{0}", niveisStr[0]);
+            } else if (n3 == 0)
+            {
+                text = String.Format("{0}-{1}", niveisStr[0], niveisStr[1]);
+            } else if (n4 == 0)
+            {
+                text = String.Format("{0}-{1}-{2}", niveisStr[0], niveisStr[1], niveisStr[2]);
+            } else if (n5 == 0)
+            {
+                text = String.Format("{0}-{1}-{2}-{3}", niveisStr[0], niveisStr[1], niveisStr[2], niveisStr[3]);
+            } else
+            {
+                text = String.Format("{0}-{1}-{2}-{3}-{4}", niveisStr[0], niveisStr[1], niveisStr[2], niveisStr[3], niveisStr[4]);
+            }
+
 
             return text;
         }
--- SistemaInterdisciplinar/frm_contas.cs
+++ SistemaInterdisciplinar/frm_contas.cs
@@ -32,30 +32,47 @@ namespace SistemaInterdisciplinar
 
             while(dr.Read())
             {
-                conta = string.Format("{0} : {1:c}", dr.GetString(7), dr.GetDouble(8));
+                
+                try
+                {
+                    n1 = (int)dr.GetInt16(1);
+                    n2 = dr.IsDBNull(2) ? 0 : (int)dr.GetInt16(2);
+                    n3 = dr.IsDBNull(3) ? 0 : (int)dr.GetInt16(2);
+                    n4 = dr.IsDBNull(4) ? 0 : (int)dr.GetInt16(4);
+                    n5 = dr.IsDBNull(5) ? 0 : (int)dr.GetInt16(5);
+
+                   
+                    string cod = ctrl.gerarClassificacao(n1, n2, n3, n4, n5);
+                    conta = "";
+                    conta = string.Format("{0} {1} : {2:c}", cod, dr.GetString(7), dr.GetDouble(8));
+                } catch ( Exception ex) {
+                    
+                    conta = string.Format("{0} : {1:c}",  dr.GetString(7), dr.GetDouble(8));
+                }
+                
                 if (dr.IsDBNull(2)) //se n2 é nulo, então a conta é de nivel 1 (n1)
                 {
                     
                     trv_contas.Nodes.Add(conta);
-                    n1++;
+                    //n1++;
                 } else if (dr.IsDBNull(3)) //se n3 é nulo, então a conta é de nivel 2 (n2)
                 {
 
                     trv_contas.Nodes[n1 - 1].Nodes.Add(conta);
-                    n2++;
+                    //n2++;
                     //trv_contas.Nodes.Add(dr.GetString(7));
                 } else if (dr.IsDBNull(4)) //se n4 é nulo, então a conta é de nivel 3 (n3)
                 {
                     trv_contas.Nodes[n1 - 1].Nodes[n2 - 1].Nodes.Add(conta);
-                    n3++;
+                    //n3++;
                 } else if (dr.IsDBNull(5)) //se n5 é nulo, então a conta é de nivel 4 (n4)
                 {
                     trv_contas.Nodes[n1 - 1].Nodes[n2 - 1].Nodes[n3 - 1].Nodes.Add(conta);
-                    n4++;
+                    //n4++;
                 } else
                 {
                     trv_contas.Nodes[n1 - 1].Nodes[n2 - 1].Nodes[n3 - 1].Nodes[n4 - 1].Nodes.Add(conta);
-                    n5++;
+                    //n5++;
                 }
 
             }
